b55ecb048435b632170928dc82b3c51a981af663
azarro777 <azarro777@gmail.com>
2021-02-09 17:25:12 +0200

    my first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import classes from "./App.module.css";
+
+function App() {
+  return (
+    <div className={classes.App}>
+      <h1>Hi, It's my app for create and purchase burgers</h1>
+    </div>
+  );
+}
+
+export default App;
